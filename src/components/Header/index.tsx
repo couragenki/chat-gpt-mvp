bd341854b138b00769fbae325cfe4fe7cf51e64c
couragenki <genki.meado@gmail.com>
2023-03-18 02:24:44 +0900

    feat: update UI

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,15 @@
+import Link from 'next/link'
 import styles from './styles.module.scss'
 
 export default function Header() {
-  return <header className={styles.headerComponent}>ヘッダー</header>;
+  return (
+    <header className={styles.headerComponent}>
+      <div>
+        <Link href="/">TOPへ戻る</Link>
+      </div>
+      <div>
+        <Link href="/apps/1">APP1へ</Link>
+      </div>
+    </header>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,44 @@
 import Head from 'next/head'
 import styles from './Top.module.scss'
-import Button from "@mui/material/Button";
 import Header from '@/components/Header';
-import Link from 'next/link';
 
 export default function TOP() {
   return (
     <>
       <Head>
-        <title>TOP</title>
+        <title>TOP PAGE</title>
       </Head>
       <Header />
-      <Link href="/apps/1">
-        <Button variant="contained">APP1へ</Button>
-      </Link>
-      <main className={styles.main}></main>
+      <main className={styles.topPage}>
+        <p>このサイトではChat-GPTの活用事例を掲載しています</p>
+        <section>
+          <div>Chat-GPT x 学習</div>
+          <ul>
+            <li>hoge</li>
+            <li>hoge</li>
+            <li>hoge</li>
+            <li>hoge</li>
+          </ul>
+        </section>
+        <section>
+          <div>Chat-GPT x 相談</div>
+          <ul>
+            <li>hoge</li>
+            <li>hoge</li>
+            <li>hoge</li>
+            <li>hoge</li>
+          </ul>
+        </section>
+        <section>
+          <div>Chat-GPT x 恋愛</div>
+          <ul>
+            <li>hoge</li>
+            <li>hoge</li>
+            <li>hoge</li>
+            <li>hoge</li>
+          </ul>
+        </section>
+      </main>
     </>
   );
 }
