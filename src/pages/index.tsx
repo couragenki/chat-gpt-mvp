3160cdcb098535675fba9baee8d47a79100fd5ea
couragenki <genki.meado@gmail.com>
2023-03-19 17:22:34 +0900

    feat: update component with MUI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,27 @@
 import styles from './Top.module.scss'
 import { PageTemplate } from '@/components/pageTemplate';
 import React from 'react';
+import Link from 'next/link';
+import { Typography } from '@mui/material';
 
 export default function TOPPAGE() {
   return (
     <div className={styles.topPage}>
       <PageTemplate>
-        <p>このサイトではChat-GPTの活用事例を掲載しています</p>
+        <Typography variant="h5" component="h2" mt={2}>
+          このサイトではChat-GPTの活用事例を掲載しています
+        </Typography>
         <section>
-          <div>Chat-GPT x 学習</div>
+          <Typography variant="h6" component="h3" mt={2}>
+            アプリ一覧
+          </Typography>
           <ul>
-            <li>hoge</li>
-            <li>hoge</li>
-            <li>hoge</li>
-            <li>hoge</li>
-          </ul>
-        </section>
-        <section>
-          <div>Chat-GPT x 相談</div>
-          <ul>
-            <li>hoge</li>
-            <li>hoge</li>
-            <li>hoge</li>
-            <li>hoge</li>
-          </ul>
-        </section>
-        <section>
-          <div>Chat-GPT x 恋愛</div>
-          <ul>
-            <li>hoge</li>
-            <li>hoge</li>
-            <li>hoge</li>
-            <li>hoge</li>
+            <li>
+              <Link href="/apps/1">アプリNo.1: チャットアプリ</Link>
+            </li>
+            <li>
+              <Link href="/apps/2">アプリNo.2: 学習レコメンド</Link>
+            </li>
           </ul>
         </section>
       </PageTemplate>
